79101728a872049543d7e8affdfc0cd28c2d6a0a
Theo Browne <me@t3.gg>
2025-02-26 00:43:44 -0800

    remove all code related to release notes and open collective

--- src/helpers/messages.ts
+++ src/helpers/messages.ts
@@ -3,10 +3,6 @@ import {
 } from 'vscode';
 
 export const MESSAGES = {
-  CHANGELOG: {
-    message: 'Material Theme was updated. Check the release notes for more details.',
-    options: {ok: 'Show me', cancel: 'Maybe later'}
-  },
   INSTALLATION: {
     message: 'Thank you for using Material Theme!'
   }
--- src/material.theme.config.ts
+++ src/material.theme.config.ts
@@ -4,21 +4,12 @@ import {
 } from 'vscode';
 
 import * as ThemeCommands from './commands';
-import {ReleaseNotesWebview} from './webviews/ReleaseNotes';
-import {changelogManager} from './core/changelog-manager';
 import {extensionManager} from './core/extension-manager';
 
 export async function activate(context: ExtensionContext): Promise<void> {
   context.globalState.setKeysForSync([extensionManager.VERSION_KEY]);
   await extensionManager.init(context);
-  const releaseNotesView = new ReleaseNotesWebview(context);
-  const installationType = extensionManager.getInstallationType();
-
-  if ((installationType.firstInstall || installationType.update) && await changelogManager.askShowChangelog()) {
-    await releaseNotesView.show();
-  }
 
   // Registering commands
   Commands.registerCommand('materialTheme.setAccent', ThemeCommands.setAccent);
-  Commands.registerCommand('materialTheme.showReleaseNotes', async () => releaseNotesView.show());
 }
